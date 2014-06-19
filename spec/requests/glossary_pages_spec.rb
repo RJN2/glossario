e75d81fedc5d7cc74ad29498df0557fe90192a86
RJN2 <rosh.code@gmail.com>
2014-06-19 21:18:47 +0100

    Refactor tests

--- spec/requests/glossary_pages_spec.rb
+++ spec/requests/glossary_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Glossary pages" do
 	describe "Index page" do
 		before { visit glossaries_path }
 
-		it { should have_content('Glossary') }
+		it { should have_selector('h1', text: 'Welcome') }
 		it { should have_title(full_title('')) }
 		it { should_not have_title("| Glossaries") }
 	end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,31 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
-
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title("| Home") }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { '' }
+  
+    it_should_behave_like "all static pages"
+    it { should_not have_title(full_title('| Home')) }
   end
 
   describe "Help page" do
-  	
     before { visit help_path }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { 'Help' }
 
-    it { should have_content('Help') }
-  	it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
   end
 end
